1164d44695f3f975577878218a92def0f442ad7c
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-04-30 09:34:56 +0530

    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ let app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:5174"],
+    origin: ["https://pubzi.onrender.com","https://pubzi-rql0.onrender.com","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
