9d97db8276078824dba7690cc19b620bc9566edd
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-05-04 10:53:56 +0530

    first commit

--- admin/src/Pages/ManageGames.jsx
+++ admin/src/Pages/ManageGames.jsx
@@ -39,7 +39,15 @@ function ManageGames() {
 
   return (
     <>
-      <div className="page-wrapper" id="main-wrapper" data-layout="vertical" >
+      <div
+        className="page-wrapper"
+        id="main-wrapper"
+        data-layout="vertical"
+        data-navbarbg="skin6"
+        data-sidebartype="full"
+        data-sidebar-position="fixed"
+        data-header-position="fixed"
+      >
         <Aside />
 
         <div className="body-wrapper">
