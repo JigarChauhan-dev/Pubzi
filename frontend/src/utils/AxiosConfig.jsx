a8aa79a95147ba62803e856c0d5fb6e7914faf5c
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-05-04 09:21:11 +0530

    first commit

--- frontend/src/utils/AxiosConfig.jsx
+++ frontend/src/utils/AxiosConfig.jsx
@@ -3,7 +3,7 @@ import CheckToken from "./CheckToken";
 import { LogoutWithoutNotification } from "./Logout";
 
 let api = axios.create({
-  baseURL:"http://localhost:8000/api"
+  baseURL:import.meta.env.VITE_API_URL
 });
 
 
