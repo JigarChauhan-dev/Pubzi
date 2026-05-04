ad04791a4cbdd225f26f69e7e6c83f35bb003507
Jigar-9958 <jigarchauhan9958@gmail.com>
2026-05-04 10:10:03 +0530

    first commit

--- frontend/src/Common/Header.jsx
+++ frontend/src/Common/Header.jsx
@@ -106,47 +106,6 @@ function Header() {
             </div>
           </div>
         </div>
-        <div className="header-right-icon">
-                  {token ? (
-                    <>
-                      <div className="header-button">
-                        <button
-                          onClick={() => {
-                            Logout();
-                          }}
-                          className="theme-btn style-2"
-                        >
-                          <span className="left-line" />
-                          Logout
-                        </button>
-                      </div>
-
-                      <div className="header-button">
-                        <Link to={"/profile"} className="theme-btn style-2">
-                          <span className="left-line" />
-                          <span className="fa fa-user"></span>
-                        </Link>
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div className="header-button">
-                        <Link to={"/login"} className="theme-btn style-2">
-                          <span className="left-line" />
-                          Login
-                        </Link>
-                      </div>
-
-                      <div className="header-button">
-                        <Link to={"/signup"} className="theme-btn style-2">
-                          <span className="left-line" />
-                          Signup
-                        </Link>
-                      </div>
-                    </>
-                  )}
-                  
-                </div>
         <div className="offcanvas__overlay" />
       </div>
 
